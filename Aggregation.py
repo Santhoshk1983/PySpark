9235298ba362372834f8222b1f5471e1a837ae83
Santhoshk1983 <santhoshk.1983@gmail.com>
2021-10-24 17:12:54 +0530

    Aggregation

--- Aggregation.py
+++ Aggregation.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import count,countDistinct, first, last
+from pyspark.sql.functions import count,countDistinct, desc, first, last,col
 
 spark = SparkSession.builder.appName("aggApp").getOrCreate()
 df = spark.read.format("csv").option("inferSchema","True").option("header","True")\
@@ -9,13 +9,16 @@ df.cache()
 print(df.count()) 
 
 # Simple grouping
-
+df.show(3)
 df.select("*").show(5)
 df.select(count("StockCode")).show()
 df.select(countDistinct("StockCode").alias("UniqueStockCodes")).show()
 df.select(first("StockCode"),last("StockCode")).show()
 
 # group by
+
+df.groupBy("Country").agg(countDistinct("CustomerID")).withColumnRenamed("count(CustomerID)","CountOfCustomer")\
+    .sort(desc("CountOfCustomer")).show(3)
 
 # Window
 
